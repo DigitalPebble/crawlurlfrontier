a7b40750f7db05b8eeb02504d0f9fdc96cf40692
Julien Nioche <julien@cameraforensics.com>
2021-07-01 12:37:13 +0100

    Added schema for metrics in ES

---
+++ ES_IndexInit.sh
@@ -0,0 +1,69 @@
+ESHOST="http://192.168.1.1:9200"
+ESCREDENTIALS="-u elastic:passwordhere"
+
+curl $ESCREDENTIALS -s -XDELETE "$ESHOST/metrics*/" >  /dev/null
+
+echo ""
+echo "Deleted metrics index"
+
+curl $ESCREDENTIALS -s -XPUT $ESHOST/_ilm/policy/7d-deletion_policy -H 'Content-Type:application/json' -d '
+{
+    "policy": {
+        "phases": {
+            "delete": {
+                "min_age": "7d",
+                "actions": {
+                    "delete": {}
+                }
+            }
+        }
+    }
+}
+'
+
+echo "Creating metrics index with mapping"
+
+# http://localhost:9200/metrics/_mapping/status?pretty
+curl $ESCREDENTIALS -s -XPOST $ESHOST/_template/storm-metrics-template -H 'Content-Type: application/json' -d '
+{
+  "index_patterns": "metrics*",
+  "settings": {
+    "index": {
+      "number_of_shards": 1,
+      "refresh_interval": "30s"
+    },
+    "number_of_replicas": 0,
+    "lifecycle.name": "7d-deletion_policy"
+  },
+  "mappings": {
+      "_source":         { "enabled": true },
+      "properties": {
+          "name": {
+            "type": "keyword"
+          },
+          "stormId": {
+            "type": "keyword"
+          },
+          "srcComponentId": {
+            "type": "keyword"
+          },
+          "srcTaskId": {
+            "type": "short"
+          },
+          "srcWorkerHost": {
+            "type": "keyword"
+          },
+          "srcWorkerPort": {
+            "type": "integer"
+          },
+          "timestamp": {
+            "type": "date",
+            "format": "date_optional_time"
+          },
+          "value": {
+            "type": "double"
+          }
+      }
+  }
+}'
+
